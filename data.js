fde10f6cac590495cdf24994c31878c059d863f0
Miguel Dias <miguelneto@univille.br>
2026-07-06 14:22:45 -0500

    Add game questions and answers data

---
+++ data.js
@@ -0,0 +1,120 @@
+/**
+ * data.js
+ * Contains all game questions and answers
+ * Easily swap out these questions for different rounds
+ */
+
+const GAME_QUESTIONS = [
+    {
+        id: 1,
+        question: "Name a popular pizza topping",
+        answers: [
+            { text: "Pepperoni", points: 45 },
+            { text: "Cheese", points: 38 },
+            { text: "Mushroom", points: 22 },
+            { text: "Onion", points: 18 },
+            { text: "Sausage", points: 15 },
+            { text: "Bacon", points: 12 },
+            { text: "Pineapple", points: 8 },
+            { text: "Olive", points: 4 }
+        ]
+    },
+    {
+        id: 2,
+        question: "Name a common breakfast food",
+        answers: [
+            { text: "Eggs", points: 52 },
+            { text: "Cereal", points: 44 },
+            { text: "Toast", points: 28 },
+            { text: "Bacon", points: 20 },
+            { text: "Pancakes", points: 18 },
+            { text: "Oatmeal", points: 12 },
+            { text: "Fruit", points: 8 },
+            { text: "Yogurt", points: 5 }
+        ]
+    },
+    {
+        id: 3,
+        question: "Name something you find in a classroom",
+        answers: [
+            { text: "Desk", points: 48 },
+            { text: "Whiteboard", points: 35 },
+            { text: "Teacher", points: 32 },
+            { text: "Students", points: 25 },
+            { text: "Books", points: 18 },
+            { text: "Clock", points: 14 },
+            { text: "Door", points: 10 },
+            { text: "Pencil", points: 6 }
+        ]
+    },
+    {
+        id: 4,
+        question: "Name a popular social media platform",
+        answers: [
+            { text: "Facebook", points: 50 },
+            { text: "Instagram", points: 42 },
+            { text: "TikTok", points: 38 },
+            { text: "Twitter", points: 28 },
+            { text: "Snapchat", points: 18 },
+            { text: "YouTube", points: 15 },
+            { text: "Reddit", points: 8 },
+            { text: "Discord", points: 5 }
+        ]
+    },
+    {
+        id: 5,
+        question: "Name a sport played with a ball",
+        answers: [
+            { text: "Basketball", points: 45 },
+            { text: "Soccer", points: 40 },
+            { text: "Football", points: 38 },
+            { text: "Baseball", points: 28 },
+            { text: "Tennis", points: 20 },
+            { text: "Volleyball", points: 15 },
+            { text: "Golf", points: 10 },
+            { text: "Ping Pong", points: 4 }
+        ]
+    },
+    {
+        id: 6,
+        question: "Name a type of weather",
+        answers: [
+            { text: "Rain", points: 55 },
+            { text: "Snow", points: 42 },
+            { text: "Sunny", points: 35 },
+            { text: "Cloudy", points: 25 },
+            { text: "Windy", points: 18 },
+            { text: "Foggy", points: 12 },
+            { text: "Thunderstorm", points: 8 },
+            { text: "Hail", points: 5 }
+        ]
+    },
+    {
+        id: 7,
+        question: "Name a color of the rainbow",
+        answers: [
+            { text: "Red", points: 48 },
+            { text: "Blue", points: 45 },
+            { text: "Yellow", points: 38 },
+            { text: "Green", points: 32 },
+            { text: "Orange", points: 22 },
+            { text: "Indigo", points: 10 },
+            { text: "Violet", points: 8 },
+            { text: "Purple", points: 5 }
+        ]
+    },
+    {
+        id: 8,
+        question: "Name a Disney character",
+        answers: [
+            { text: "Mickey Mouse", points: 52 },
+            { text: "Cinderella", points: 40 },
+            { text: "Elsa", points: 38 },
+            { text: "Simba", points: 30 },
+            { text: "Ariel", points: 22 },
+            { text: "Buzz Lightyear", points: 16 },
+            { text: "Snow White", points: 10 },
+            { text: "Moana", points: 6 }
+        ]
+    }
+];
